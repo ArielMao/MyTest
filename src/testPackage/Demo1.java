70e657ae3b57efb2467e7b4d55f330133d565dbe
ArielMao <30836469+ArielMao@users.noreply.github.com>
2017-08-08 23:23:57 +0800

    我就试试GITHUB怎么用

---
+++ src/testPackage/Demo1.java
@@ -0,0 +1,43 @@
+package testPackage;
+
+import java.util.Scanner;
+
+public class Demo1 {
+
+	public static void main(String[] args){
+		
+		/*例1
+		System.out.print("请输入分数：");
+		Scanner input = new Scanner(System.in);//获取用户输入数据
+		int score = input.nextInt();//将获取的数据作为int类型复制给score
+		System.out.println("加分前的分数是："+score);
+		int count=0;
+		while(score<60){
+			score++;
+			count++;
+		}
+		System.out.println("加分后的分数是："+score);
+		System.out.println("一共加了"+count+"次数");
+		*/
+		// 创建一个空的StringBuilder对象
+		/*例2
+				StringBuilder str = new StringBuilder();
+		        
+				// 追加字符串
+				str.append("jaewkjldfxmopzdm");
+				
+		        // 从后往前每隔三位插入逗号
+				for(int i = 1; i<str.length();i=i+4){
+		                str.insert(i, ',');
+		                System.out.print(i + ":" + str.toString() + "\n");
+				}
+		        
+		        // 将StringBuilder对象转换为String对象并输出
+				//System.out.print(str.toString());
+				 
+				 */
+		/*例3
+		 *
+		 */
+	}
+}
---
+++ src/testPackage/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author ariel
+ *
+ */
+package testPackage;
